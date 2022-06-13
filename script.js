b3c7b3d0dda1b409ac3ce260ca5ac6dd1ae7c57a
Zismant <Zismantt@gmail.com>
2022-06-13 12:53:16 +0300

    my

--- script.js
+++ script.js
@@ -1,63 +1,22 @@
 'use strict';
 
-// let calc = new Calculator();
+function Calculator() {
+    this.calculate = function(str) {
+        let arr = str.split(' ');
+        this.a = +arr[0];
+        this.b = +arr[2];
+        this.operation = arr[1];
+        console.log(arr);
 
-// function Calculator() {
-//     this.name = '';
-//     this.methods = {};
+        if (this.operation === '+') {return this.plus();}
+        if (this.operation === '-') {this.minus();}
 
+    };
 
-//     this.calculate = function (str) {
+    this.plus = () => this.a + this.b;
+    this.plus = () => this.a + this.b;
 
-//         let arr = str.split(' ');
-//         let a = arr[0];
-//         let b = arr[arr.length - 1];
-//         let op = arr[1]; // "+" 
-
-//         if (this.methods[op]) {
-//             //+
-//         let func = this.methods[op];
-//            return func[this.op](a, b);
-//         } else { console.warn("there are not methods. please add somr methods")}
-        
-
-
-
-//     };
-
-//     this.addMethod = function (op, fn) {
-//         this.map[op] = fn;
-//         //{"+" : (a, b) => a + b
-//     //     "-" : (a, b) => a - b }
-//     };
-
-
-// }
-
-// let powerCalc = new Calculator();
-// powerCalc.addMethod("+", );
-// powerCalc.addMethod("-", (a, b) => a - b);
-// powerCalc.addMethod("*", (a, b) => a * b);
-// powerCalc.addMethod("/", (a, b) => a / b);
-// powerCalc.addMethod("**", (a, b) => a ** b);
-// //powerCalc.addMethod("send na hui", (a, b) => console.log(`params ${a} send na hui params ${b}`))
-// console.log(powerCalc.calculate("2 ** 3"));
-
-let obj = {};
-
-function createObj(key, value) {
-   obj[key] = value;
-    
-    
 }
 
-let a = createObj('age', 88 );
-console.log(obj);
-createObj('agerr', 88 );
-console.log(obj);
-createObj('agerruu', 887 );
-console.log(obj);
-createObj('agerruuu', 887 );
-
-console.log(obj);
-
+    let calc = new Calculator();
+    console.log(calc.calculate("3 - 7"));
