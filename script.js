362d83fc1efbf4ee8739c852317edb3209b0b930
Zismant <Zismantt@gmail.com>
2022-08-27 12:07:28 +0300

    passwordGenrrator

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 'use strict';
 
-function formatDate(date) {
-  
-  let dif = new Date() - date;
-  let difSec = Math.round(dif / 1000);
-
-  if (difSec < 1) {return 'прямо сейчас';}
-
-  let difMinut = difSec / 60;
-  if (difMinut < 1) {return `${difSec} сек. назад`;}
-
-  let difHour = difMinut / 60;
-  if (difHour < 1) {return `${difMinut} мин. назад`;}
-
-  let d = {};
-  d.date = date.getDate();
-  d.date = d.date < 10 ? '0' + d.date.toString() : d.date;
-  d.month = date.getMonth() + 1;
-  d.month = d.month < 10 ? '0' + d.month.toString() : d.month;
-  d.year = date.getFullYear().toString().slice(2);
-  d.hour = date.getHours();
-  d.hour = d.hour < 10 ? '0' + d.hour.toString() : d.hour;
-  d.minut = date.getMinutes();
-  d.minut = d.minut < 10 ? '0' + d.minut.toString() : d.minut;
-
-  return `${d.date}.${d.month}.${d.year} ${d.hour}:${d.minut}`;
+function c(...arg) {
+    console.log(...arg);
 }
 
-console.log(formatDate(new Date(new Date() - 1)));
-console.log(formatDate(new Date(new Date() - 30 * 1000)));
-console.log(formatDate(new Date(new Date() - 5 * 60 * 1000)));
-console.log(formatDate(new Date(new Date() - 84600 * 1000 )));
+function alert(...arg) {
 
+    console.log(...arg);
+}
 
+function generatePasswordCode(length) {
+    const min1 = 48;
+    const max1 = 57;
 
+    const min2 = 65;
+    const max2 = 90;
+
+    const min3 = 97;
+    const max3 = 122;
+
+    function charCode() {
+
+        function random(min, max) {
+            return Math.floor(Math.random() * (max + 1 - min) + min);
+        }
+        let arr = [];
+        arr.push(random(min1, max1));
+        arr.push(random(min2, max2));
+        arr.push(random(min3, max3));
+
+        return arr[random(0, arr.length - 1)];
+        
+    }
+
+    let res = [];
+    for (let i = 0; i < length; i++) {
+        res.push(String.fromCharCode(charCode()));
+    }
+
+    return c(res.join(''));
+
+}
+
+generatePasswordCode(17);
