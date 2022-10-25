d7d29811eeb6e54ac6d3f447875c1d7ffa4eaf94
Zismant <Zismantt@gmail.com>
2022-10-25 13:39:59 +0300

    slider

--- script.js
+++ script.js
@@ -5,41 +5,46 @@ function c(...arg) {
 }
 
 function alert(...arg) {
-
     console.log(...arg);
 }
 
-function generatePasswordCode(length) {
-    const min1 = 48;
-    const max1 = 57;
 
-    const min2 = 65;
-    const max2 = 90;
+const img = document.querySelectorAll('img'),
+      prev = document.querySelector('#prev'),
+      next = document.querySelector('#next');
+      
 
-    const min3 = 97;
-    const max3 = 122;
+let sliderLength = img.length;
+document.querySelector('.length').textContent = sliderLength ;
+let index = 1;
 
-    function charCode() {
+show();
 
-        function random(min, max) {
-            return Math.floor(Math.random() * (max + 1 - min) + min);
-        }
-        let arr = [];
-        arr.push(random(min1, max1));
-        arr.push(random(min2, max2));
-        arr.push(random(min3, max3));
-
-        return arr[random(0, arr.length - 1)];
-        
-    }
-
-    let res = [];
-    for (let i = 0; i < length; i++) {
-        res.push(String.fromCharCode(charCode()));
-    }
-
-    return c(res.join(''));
-
+function nextSlaid() {
+    if (index >= sliderLength) {index = 1;}
+    else {index++;}
+    show();
+    
 }
 
-generatePasswordCode(17);
+function prevSlaid() {
+    if (index <= 1) {index = sliderLength;}
+    else {index--;}
+    show();
+   
+}
+
+function show() {
+    
+    img.forEach(item => {
+        item.style.display = 'none';
+    });
+    img[index - 1].style.display = '';
+    document.querySelector('.index').textContent = index;
+}
+
+prev.addEventListener('click', () => prevSlaid());
+next.addEventListener('click', () => nextSlaid());
+
+
+
---
+++ test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+ function c(...arg) {
+    console.log(...arg);
+}
+
+ function alert(...arg)     {
+    console.log(...arg);
+}
+
